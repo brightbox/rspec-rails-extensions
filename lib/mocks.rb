e34016e269d4fcff078f65ca6572f61e209220ae
Rahoul Baruah <rahoul@brightbox.co.uk>
2009-02-16 10:13:56 +0000

    got rid of invalid characters, changed version number

--- lib/mocks.rb
+++ lib/mocks.rb
@@ -1,24 +1,27 @@
 module Spec
-  module Rails
-    module Mocks
-      # Set up the given mock model in preparation for an ActiveRecord::RecordInvalid call
-      # For example: 
-      #   @user = mock_model User
-      #   prepare_for_errors_on @user
-      #   @user.should_receive(:save!).and_raise(ActiveRecord::RecordInvalid.new(@user))
-      # If you tried this without the call to prepare_for_errors_on, the RecordInvalid exception would not be raised.  
-      def prepare_for_errors_on model
-        errors = mock 'errors'
-        errors.should_receive(:full_messages).and_return([])
-        model.should_receive(:errors).and_return(errors)
-      end
-
-      # Shortcut to create a mock model that will have ActiveRecord::RecordInvalid
+  module Rails
+    module Mocks
+      # Set up the given mock model in preparation for an ActiveRecord::RecordInvalid call
+      # For example: 
+      #   @user = mock_model User
+      #   prepare_for_errors_on @user
+      #   @user.should_receive(:save!).and_raise(ActiveRecord::RecordInvalid.new(@user))
+      # If you tried this without the call to prepare_for_errors_on, the RecordInvalid exception would not be raised.  
+      def prepare_for_errors_on model
+        errors = mock 'errors'
+        errors.should_receive(:full_messages).and_return([])
+        model.should_receive(:errors).and_return(errors)
+      end
+    
+      # Shortcut to create a mock model that is prepared for a ActiveRecord::RecordInvalid.new(@model) call
+      # Equivalent to: 
+      #   @model = mock_model Model, :some => :fields
+      #   prepare_for_errors_on @model
       def invalid_model class_name, stubs = {}
         model = mock_model class_name, stubs
         prepare_for_errors_on model
         return model
       end
-    end
-  end
+    end
+  end
 end
