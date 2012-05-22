0339032855b387426e0bea75f6c6577fae1d767f
Hemant Kumar <gethemant@gmail.com>
2012-05-22 15:28:30 +0530

    cleanup whitespaces

--- lib/controller_example_group.rb
+++ lib/controller_example_group.rb
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 module Spec
   module Rails
     module Example
       class ControllerExampleGroup < FunctionalExampleGroup
-        # Perform a pseudo HTTP GET, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.   
-        # For example: 
+        # Perform a pseudo HTTP GET, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.
+        # For example:
         #   on_getting :show, :id => '25' do
         #     @person = mock_model Person
         #     Person.should_receive(:find).with('25').and_return(@person)
@@ -24,7 +25,7 @@ module Spec
           end
         end
 
-        # Perform a pseudo HTTP POST, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.   
+        # Perform a pseudo HTTP POST, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.
         # As with on_getting, use :as_xhr => true to make this an XMLHTTPRequest
         def on_posting_to page, parameters = {}
           yield if block_given?
@@ -35,8 +36,8 @@ module Spec
             post page, parameters
           end
         end
-        
-        # Perform a pseudo HTTP PUT, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.   
+
+        # Perform a pseudo HTTP PUT, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.
         # As with on_getting, use :as_xhr => true to make this an XMLHTTPRequest
         def on_putting_to page, parameters = {}
           yield if block_given?
@@ -47,8 +48,8 @@ module Spec
             put page, parameters
           end
         end
-        
-        # Perform a pseudo HTTP DELETE, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.   
+
+        # Perform a pseudo HTTP DELETE, performing any code in the block beforehand.  This is to allow you to set up expectations on your mocks whilst keeping the code readable.
         # As with on_getting, use :as_xhr => true to make this an XMLHTTPRequest
         def on_deleting_from page, parameters = {}
           yield if block_given?
