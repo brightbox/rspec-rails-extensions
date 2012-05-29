8fc71778e535ad70abc438a8fd2c473a34993b04
Hemant Kumar <gethemant@gmail.com>
2012-05-29 20:36:50 +0530

    add support for mock count

--- lib/mocks.rb
+++ lib/mocks.rb
@@ -7,10 +7,10 @@ module RSpec
       #   prepare_for_errors_on @user
       #   @user.should_receive(:save!).and_raise(ActiveRecord::RecordInvalid.new(@user))
       # If you tried this without the call to prepare_for_errors_on, the RecordInvalid exception would not be raised.
-      def prepare_for_errors_on model
+      def prepare_for_errors_on(model,count = 1)
         errors = mock 'errors'
         errors.should_receive(:full_messages).and_return([])
-        model.should_receive(:errors).and_return(errors)
+        model.should_receive(:errors).at_least(count).times.and_return(errors)
       end
 
       # Shortcut to create a mock model that is prepared for a ActiveRecord::RecordInvalid.new(@model) call
